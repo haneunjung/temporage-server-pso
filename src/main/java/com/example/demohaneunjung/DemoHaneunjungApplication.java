dab1fd77a5dbc51cd8df34bc7f136e3a5f078fbb
sang5c <skah321@gmail.com>
2020-02-12 00:23:27 +0900

    first commit

---
+++ src/main/java/com/example/demohaneunjung/DemoHaneunjungApplication.java
@@ -0,0 +1,13 @@
+package com.example.demohaneunjung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHaneunjungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoHaneunjungApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demohaneunjung/DemoHaneunjungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demohaneunjung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHaneunjungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
