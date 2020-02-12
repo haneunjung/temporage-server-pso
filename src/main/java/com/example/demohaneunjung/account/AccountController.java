19163a4596174406c6fae3813f63598b331fb264
sang5c <skah321@gmail.com>
2020-02-13 00:26:42 +0900

    spring security 로그인 처리 로직 추가

--- src/main/java/com/example/demohaneunjung/account/AccountController.java
+++ src/main/java/com/example/demohaneunjung/account/AccountController.java
@@ -1,11 +1,27 @@
 package com.example.demohaneunjung.account;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class AccountController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AccountService accountService;
 
     @PostMapping("/sign-up")
     public String signUp() {
@@ -13,8 +29,14 @@ public class AccountController {
     }
 
     @PostMapping("/sign-in")
-    public String signIn() {
-        return "";
+    public UserDetails signIn(@RequestBody Account account, HttpSession httpSession) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(account.getUsername(), account.getPassword()); // 인증 토큰 생성
+        Authentication authentication = authenticationManager.authenticate(token); // 실질적인 ID / PWD 인증
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        httpSession.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+
+        return accountService.loadUserByUsername(account.getUsername());
     }
 
     @GetMapping("/test")
